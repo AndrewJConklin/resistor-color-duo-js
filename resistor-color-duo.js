d8e925d4bc6b011bbb1391c78aea55832ad9def1
aconkli9 <aconkli9@ford.com>
2022-04-26 11:50:44 -0400

    Add solution

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -1,1 +1,20 @@
-export function decodedValue() {}
+export const colorList = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+}
+
+export function decodedValue(colors) {
+    let id = ""
+    for (let i = 0; i < 2; i++) {
+        id = id + `${colorList[colors[i]]}`
+    }
+    return +id
+}
